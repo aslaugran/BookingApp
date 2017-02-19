a447c647cf6b2c718f88a15cd63084a84de5ec99
Ása Einarsdóttir <alsaugran@gmail.com>
2017-02-19 15:55:14 +0000

    ...

--- index.ios.js
+++ index.ios.js
@@ -317,10 +317,11 @@ class ToursCategory extends Component {
         super();
         const onButtonPress = () => {
         Alert.alert('Button has been pressed!');
+        this.state.scenes({scenes: 'Adventure'});
         };
   }
 
-  render() {
+  render(){
     return (
         <View style={styles.tourcategorycard}>
           <Image source={{uri: this.props.category.photo}}
@@ -342,6 +343,67 @@ class ToursCategory extends Component {
           </View>
     )
 
+  }
+}
+
+class TourSingleModal extends Component {
+  state = {
+    modalVisible: false,
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+  render() {
+    return (
+  <View style={{marginTop: 22}}>
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+          >
+         <ScrollView>
+          <View>
+            <Image source={{uri: 'https://www.re.is/media/tour-headers/640/RE04Mobile.jpg'}}
+              style={styles.singletourimage} />
+              <TouchableHighlight onPress={() => {
+                this.setModalVisible(!this.state.modalVisible)
+              }}><Text style={styles.closemodals}>X</Text>
+              </TouchableHighlight>
+            <Text style={styles.singetourname}>The Golden Circle</Text>
+            <View style={styles.singletourwrap}>
+            <Text style={styles.singletourintro}>Glacier hike through a
+            wonderland of ice sculptures</Text>
+            <View style={styles.line}></View>
+            <View style={styles.infoleft}>
+              <Text style={styles.singletourprice}>24.900 ISK</Text>
+            </View><View style={styles.inforight}>
+              <Text style={styles.toursingleduration}>Duration: 8 hours</Text>
+            </View>
+            <Text style={styles.toursingledec}>The Golden Circle tour allows you to visit some of Iceland’s most stunning sights, starting with the Geysir geothermal area where the Strokkur geyser shoots a column of water up to 30 metres (98 ft.) into the air every 4-8 minutes in a thrilling display of nature’s forces. The visit continues with Gullfoss (Golden Falls) waterfall, created by the river Hvítá, which tumbles and plunges into a crevice some 32 m (105 ft.) deep. The Golden Circle tour also includes the historical and geological wonder that is Thingvellir National Park, where the American and Eurasian tectonic plates are pulling apart at a rate of a few centimetres per year. Additionally, the tour includes a visit to the idyllic Friðheimar greenhouse cultivation centre, where you can learn about the magic behind growing delicious, pesticide-free tomatoes and cucumbers with the aid of the geothermal heat that Iceland has in abundance.</Text>
+          </View>
+          </View>
+         </ScrollView>
+         <View style={styles.booknow}>
+           <Button
+         onPress={onButtonPress}
+         title="Book Now"
+         accessibilityLabel="More Information"
+         color='white'
+         fontSize='26'
+       />
+         </View>
+        </Modal>
+
+        <TouchableHighlight onPress={() => {
+          this.setModalVisible(true)
+        }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+
+      </View>
+    );
   }
 }
 
@@ -429,70 +491,106 @@ class TourCard extends Component {
   }
   render() {
     return (
-      <View style={styles.tourcard}>
+      <ScrollView style={styles.tourcard}>
         {/* <Image source={{uri: this.state.main_photo}}
           style={styles.tourimage} /> */}
         <Text style={styles.tourname}>
           {this.state.data.length>0?this.state.data[this.props.tour.i].name: null}
         </Text>
-      </View>
+      </ScrollView>
     );
   }
 }
 
-// class BookingApp extends Component {
-//   constructor (){
-//     super();
-//     this.getCategories();
-//         this.state = {
-//           data: []
-//         }
-//     }
-//
-//     getCategories (){
-//         return fetch('http://localhost:3001/daytours/categories')
-//           .then((response) => response.json())
-//           .then((responseJson) => {
-//             this.setState ({data: responseJson});
-//             return responseJson.categories;
-//           })
-//           .catch((error) => {
-//             console.error(error);
-//           });
-//       }
-//   render() {
-//           const routes = [
-//             {title: 'Our Day Tours Selection', index: 0},
-//             {title: 'Adventure',  index: 1},
-//           ];
-//           return (
-//       <Navigator style={styles.navigator}
-//       initialRoute={routes[0]}
-//       initialRouteStack={routes}
-//       renderScene={(route, navigator) =>
-//         <TouchableHighlight onPress={() => {
-//           if (route.index === 0) {
-//             navigator.push(routes[1]);
-//           } else {
-//             navigator.pop();
-//           }
-//         }}>
-//         <Text style={styles.categoryheading}>{route.title}</Text>
-//         </TouchableHighlight>
-//       }
-//       style={{padding: 100}}
-//     />
-//   );
-//       }
-// }
-
-
 // Styles
 const styles = StyleSheet.create({
+  //TourCardStyle
+  tourcard: {
+    width: 313,
+    height: 190,
+    borderRadius: 10,
+    marginBottom: 20,
+   backgroundColor: '#6C6C6C',
+  },
+  tourname: {
+    fontSize: 17,
+    fontWeight: '600',
+    lineHeight: 25,
+    color: '#fff',
+    textAlign: 'left',
+    marginTop: 60,
+    marginLeft: 20,
+  },
   maincontainer: {
     backgroundColor: '#f2f2f2',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  booknow: {
+    height: 68,
+    width: 375,
+    backgroundColor: '#81C17D',
+    paddingTop: 15,
+  },
+  line: {
+    width: 313,
+    height: 3,
+    backgroundColor: '#fe5c5c',
+    marginBottom: 5,
+    marginTop: 5,
+  },
+  infoleft: {
+    width: 155,
+  },
+  inforight: {
+    width: 155,
+  },
+  singletourwrap: {
+    padding: 32,
+  },
+  singletourintro: {
+    fontSize: 22,
+    fontWeight: '600',
+    lineHeight: 26,
+    color: "#6c6c6c",
+  },
+  singletourprice: {
+    fontSize: 16,
+    color: "#6c6c6c",
+    fontWeight: '900',
+    textAlign: 'left',
+  },
+  toursingleduration: {
+    fontSize: 16,
+    color: "#6c6c6c",
+  },
+  toursingledec: {
+    fontSize: 17,
+    color: "#6c6c6c",
+    lineHeight: 25,
+    paddingTop: 15,
+  },
+  closemodals: {
+    color: 'black',
+    fontSize: 20,
+    position: 'relative',
+    left: 20,
+    paddingTop: 5,
+  },
+  singletourimage: {
+    width: 600,
+    padding: 0,
+    height: 250,
+  },
+  singetourname: {
+    fontSize: 30,
+    fontWeight: '900',
+    position: 'absolute',
+    right: 110,
+    top: 60,
+    width: 250,
+    color: 'white',
+    backgroundColor: 'transparent',
   },
   buttoncontainer: {
     backgroundColor: '#4A4A4A',
